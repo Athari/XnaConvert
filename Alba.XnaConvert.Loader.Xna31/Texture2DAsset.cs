491eb8e69752c49db802692ee6f3d0f1c88f4f2f
Athari <athari.p@gmail.com>
2013-07-04 05:30:51 +0400

    Added support for XNA 3.1

---
+++ Alba.XnaConvert.Loader.Xna31/Texture2DAsset.cs
@@ -0,0 +1,20 @@
+﻿using Alba.XnaConvert.Common;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Alba.XnaConvert.Loader.Xna31
+{
+    public class Texture2DAsset : IAsset
+    {
+        private readonly Texture2D _asset;
+
+        public Texture2DAsset (Texture2D asset)
+        {
+            _asset = asset;
+        }
+
+        public void SaveToFile (string filename)
+        {
+            _asset.Save(filename, ImageFileFormat.Png);
+        }
+    }
+}
